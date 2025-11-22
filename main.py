340d87f3f1cb6431ddccc2e9965c0b606efbab20
n205 <54013649+n205@users.noreply.github.com>
2025-11-22 19:18:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-main()
+import pandas as pd
+import gspread
+import logging
+
+# Cloud Logging に出力するよう設定
+logging.basicConfig(level=logging.INFO)
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    logging.info('📥 リクエスト受信')
+
+  
+    return 'Cloud Run Function executed.', 200
+
+
+if __name__ == '__main__':
+    logging.info('🚀 アプリ起動')
+    app.run(host='0.0.0.0', port=8080)
