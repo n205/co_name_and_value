6ca326fec9a7d2e0567b7b1920c14e70c84bb7e6
n205 <54013649+n205@users.noreply.github.com>
2025-11-22 19:45:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+from flask import Flask
 import pandas as pd
 import gspread
+from gspread_dataframe import get_as_dataframe
+from google.oauth2 import service_account
 import logging
+
+from read_sheet import read_sheet
 
 # Cloud Logging に出力するよう設定
 logging.basicConfig(level=logging.INFO)
+
+app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     logging.info('📥 リクエスト受信')
 
-  
+    # スプレッドシート読込
+    worksheet, existing_df, processed_urls = read_sheet()
+
     return 'Cloud Run Function executed.', 200
 
 
