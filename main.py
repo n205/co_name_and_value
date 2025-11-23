7fc991446f53cdb736887055d51d74f4e35eabfd
n205 <54013649+n205@users.noreply.github.com>
2025-11-23 18:16:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ def main():
     # スプレッドシート読込
     worksheet, existing_df, processed_urls = read_sheet()
 
-    update_組織名T(worksheet)
-    update_組織名G(worksheet)
-    update_組織名(worksheet)    
+    #update_組織名T(worksheet)
+    #update_組織名G(worksheet)
+    #update_組織名(worksheet)    
     update_証券番号(worksheet)
     
     return 'Cloud Run Function executed.', 200
