e445b8692512d6c75288f6fd45031c1560fc0006
n205 <54013649+n205@users.noreply.github.com>
2025-11-23 17:33:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from read_sheet import read_sheet
 from update_組織名 import update_組織名T
 from update_組織名 import update_組織名G
 from update_組織名 import update_組織名
+from update_組織名 import update_証券番号
 
 
 # Cloud Logging に出力するよう設定
@@ -26,7 +27,8 @@ def main():
     update_組織名T(worksheet)
     update_組織名G(worksheet)
     update_組織名(worksheet)    
-
+    update_証券番号(worksheet)
+    
     return 'Cloud Run Function executed.', 200
 
 
